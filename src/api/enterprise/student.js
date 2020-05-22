20733999c0bd944c4ee0f1e41ca3332bc57784f8
cuixiaoyan <56696508@qq.com>
2020-05-22 17:05:25 +0800

    增加学生模块，练习代码生成，权限配置。

---
+++ src/api/enterprise/student.js
@@ -0,0 +1,53 @@
+import request from '@/utils/request'
+
+// 查询学生信息列表
+export function listStudent(query) {
+  return request({
+    url: '/enterprise/student/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询学生信息详细
+export function getStudent(studentId) {
+  return request({
+    url: '/enterprise/student/' + studentId,
+    method: 'get'
+  })
+}
+
+// 新增学生信息
+export function addStudent(data) {
+  return request({
+    url: '/enterprise/student',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改学生信息
+export function updateStudent(data) {
+  return request({
+    url: '/enterprise/student',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除学生信息
+export function delStudent(studentId) {
+  return request({
+    url: '/enterprise/student/' + studentId,
+    method: 'delete'
+  })
+}
+
+// 导出学生信息
+export function exportStudent(query) {
+  return request({
+    url: '/enterprise/student/export',
+    method: 'get',
+    params: query
+  })
+}
